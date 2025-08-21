ccf4c40bd4545622d0ea2105f2f5866c9a2e524b
Stratiev1 <135167059+Stratiev1@users.noreply.github.com>
2025-08-21 10:50:53 +0300

    Remove file input element

--- src/components/client/BuyProductPanel.tsx
+++ src/components/client/BuyProductPanel.tsx
@@ -238,13 +238,6 @@ export const BuyProductPanel: React.FC = () => {
                   Upload Files
                 </label>
                 <div className="border-2 border-dashed border-gray-300 rounded-md p-4">
-                  <input
-                    type="file"
-                    onChange={handleFileUpload}
-                    multiple
-                    className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-primary-orange focus:border-transparent"
-                    id="file-upload"
-                  />
                   <label
                     htmlFor="file-upload"
                     className="cursor-pointer flex flex-col items-center"
